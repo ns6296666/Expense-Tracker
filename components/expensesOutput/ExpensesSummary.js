2f07f3e91a1a41f0aeba875a995f2dbac999b159
Harry <harry@macbookpro>
2024-01-31 10:37:37 +0000

    creating component to diaplay expenses

---
+++ components/expensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+function ExpensesSummary() {
+  return (
+    <View>
+      <Text>last 7 days:</Text>
+      <Text>$177.95</Text>
+    </View>
+  );
+}
+
+export default ExpensesSummary;
